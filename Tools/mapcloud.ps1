b193b1a1bc6d1d2cc3f9ac5240dd183b6858048d
Andrii Pylypenko <andrii.pylypenko@gws.ms>
2022-09-17 17:45:14 +0200

    add maping scripts

---
+++ Tools/mapcloud.ps1
@@ -0,0 +1,3 @@
+New-Item -ItemType SymbolicLink -Path "k:\AosService\PackagesLocalDirectory\NDinject" -Target "NDinject"
+New-Item -ItemType SymbolicLink -Path "k:\AosService\PackagesLocalDirectory\NDinjectExamples" -Target "NDinjectExamples"
+New-Item -ItemType SymbolicLink -Path "k:\AosService\PackagesLocalDirectory\NDinjectTests" -Target "NDinjectTests"
---
+++ Tools/unmapcloud.ps1
@@ -0,0 +1,3 @@
+(Get-Item "k:\AosService\PackagesLocalDirectory\NDinject").Delete()
+(Get-Item "k:\AosService\PackagesLocalDirectory\NDinjectExamples").Delete()
+(Get-Item "k:\AosService\PackagesLocalDirectory\NDinjectTests").Delete()
